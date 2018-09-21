0a8339cd4c718c4fa314bf24550237ced7c1845f
Masen Matthews <masenmatthews@gmail.com>
2018-09-20 17:06:05 -0700

    add router, add ticket list component, disable warnings for indentation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import TicketList from './TicketList';
 import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+import NewTicketForm from './NewTicketForm';
 
 function App(){
   return (
     <div>
       <Header/>
-      <TicketList/>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+      <Switch>
+      </Switch>
+
     </div>
   );
 }
 
 export default App;
+
+// router path previously defined as <TicketList/>
+// route exact path defined as exact because it contains a portion of another route. this is really weird but apparently react router is being changed so that all paths are defined with the 'exact' tag.
