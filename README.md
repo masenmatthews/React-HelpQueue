2a6dda1671fed4ccaeaec86159a77584f89103f9
Masen Matthews <masenmatthews@gmail.com>
2018-09-22 16:56:19 -0700

    add event handler for clicking text

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Launch server by running:
   $ webpack-dev-server
 
 localhost:8080
+
+Steps to adding statefulness:
+
+1. Create a stateful class-based component that will record whether our app is in the state of displaying questions, or the state of displaying the form.
+
+2. Construct an event to toggle our state value when the user completes our questions. (This event will later be refactored in step 5.)
+
+3. Create a new component to contain the questions we want to appear before the form.
+
+4. Use JSX conditional rendering to change what's displayed depending on the current state.
+
+5. Learn about unidirectional data flow to allow components to communicate and work in sync. (And refactor the event we created in step two.)
--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -7,12 +7,19 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState({formVisibleOnPage: true});
+    console.log('Hey, you clicked me! I do not have code to change my state quite yet, but I will in a moment!');
   }
 
   render(){
     return (
       <div>
         <p>This is the NewTicketControl component!</p>
+        <strong onClick={this.handleClick}>Click me to change my state!</strong>
       </div>
     );
   }
@@ -23,3 +30,7 @@ export default NewTicketControl;
 // super keyword called to access a parent class's constructor. In this case, that's the React.Component constructor.
 
 // this.state = {}; is standard convention for delcaring state in ES6. More than one state value could be added by using key-value pairs
+
+// onClick means that the attached method will be triggered when the cluser clicks the text
+
+// this.handleClick states that a method from this component called handleClick() will be triggered when the lements is clicked
