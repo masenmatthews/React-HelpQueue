647175f452d05fdb07f19af7f7e00cd1e40a1b95
Masen Matthews <masenmatthews@gmail.com>
2018-10-11 14:42:26 -0700

    continue redux implementation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,22 +3,21 @@ import Header from './Header';
 import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import Moment from 'moment';
 import Admin from './Admin';
-import { v4 } from 'uuid';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      masterTicketList: {},
       selectedTicket: null
     };
     this.handleChangingSelectedTicket = this.handleChangingSelectedTicket.bind(this);
   }
-
 
   componentDidMount() {
     this.waitTimeUpdateTimer = setInterval(() =>
@@ -32,28 +31,25 @@ class App extends React.Component {
   }
 
   updateTicketElapsedWaitTime() {
-    var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
-    Object.keys(newMasterTicketList).forEach(ticketId => {
-      newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
-    });
-    this.setState({masterTicketList: newMasterTicketList});
+    // var newMasterTicketList = Object.assign({}, this.state.masterTicketList);
+    // Object.keys(newMasterTicketList).forEach(ticketId => {
+    //   newMasterTicketList[ticketId].formattedWaitTime = (newMasterTicketList[ticketId].timeOpen).fromNow(true);
+    // });
+    // this.setState({masterTicketList: newMasterTicketList});
   }
-
-  // The Object.assign() method is used to copy the values of all enumerable own properties from one or more source objects to a target object. It will return the target object.
 
   handleChangingSelectedTicket(ticketId){
     this.setState({selectedTicket: ticketId});
   }
 
   render(){
-    console.log(this.state.masterTicketList);
     return (
       <div>
         <Header/>
         <Switch>
-          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route exact path='/' render={()=><TicketList ticketList={this.props.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl />} />
-          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname}
+          <Route path='/admin' render={(props)=><Admin ticketList={this.props.masterTicketList} currentRouterPath={props.location.pathname}
             onTicketSelection={this.handleChangingSelectedTicket}
             selectedTicket={this.state.selectedTicket}/>} />
           <Route component={Error404} />
@@ -61,7 +57,16 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
-export default App;
+App.propTypes = {
+  masterTicketList: PropTypes.object
+};
+
+const mapStateToProps = state => {
+  return {
+    masterTicketList: state
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(App));
--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ConfirmationQuestions from './ConfirmationQuestions';
 import NewTicketForm from './NewTicketForm';
+import { dispatch } from 'react-redux';
 
 class NewTicketControl extends React.Component {
 
@@ -19,7 +20,7 @@ class NewTicketControl extends React.Component {
   render(){
     let currentlyVisibleContent = null;
     if (this.state.formVisibleOnPage){
-          currentlyVisibleContent = <NewTicketForm onNewTicketCreation={this.props.onNewTicketCreation}/>;
+      currentlyVisibleContent = <NewTicketForm />;
     } else {
       currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
     }
@@ -31,20 +32,4 @@ class NewTicketControl extends React.Component {
   }
 }
 
-NewTicketControl.propTypes = {
-  onNewTicketCreation: PropTypes.func
-};
-
-// Everything in the render function is related to what's being displayed. currentlyVisibleContent gets filled with the new ticket form or the confirmation questions.
-
-// Event handler will work with the new conditional rendering when unidirectional data flow is introduced.
-
 export default NewTicketControl;
-
-// super keyword called to access a parent class's constructor. In this case, that's the React.Component constructor.
-
-// this.state = {}; is standard convention for delcaring state in ES6. More than one state value could be added by using key-value pairs
-
-// onClick means that the attached method will be triggered when the cluser clicks the text
-
-// this.handleClick states that a method from this component called handleClick() will be triggered when the lements is clicked
